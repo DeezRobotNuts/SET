5edcd76ba0627f622d626d80a22c8f5d7c2fa0d7
DeezRobotNuts <48024970+DeezRobotNuts@users.noreply.github.com>
2024-01-24 20:48:28 +0100

    Add files via upload

---
+++ class kaarten 2de manier.py
@@ -0,0 +1,67 @@
+def verglijk_nummer(kaart1, kaart2, kaart3): 
+     if kaart1.nummer == kaart2.nummer == kaart3.nummer or (kaart1.nummer != kaart2.nummer and kaart1.nummer != kaart3.nummer and kaart2.nummer != kaart3.nummer):
+        return True
+     else:
+        return False
+
+def verglijk_symbool(kaart1, kaart2, kaart3): 
+     if (kaart1.symbool == kaart2.symbool == kaart3.symbool or 
+         (kaart1.symbool != kaart2.symbool and kaart1.symbool != kaart3.symbool and kaart2.symbool != kaart3.symbool)):
+        return True
+     else:
+        return False
+
+def verglijk_kleur(kaart1, kaart2, kaart3):
+     if (kaart1.kleur == kaart2.kleur == kaart3.kleur or (kaart1.kleur != kaart2.kleur and kaart1.kleur != kaart3.kleur and kaart2.kleur != kaart3.kleur)):
+        return True
+     else:
+        return False
+def verglijk_shading(kaart1, kaart2, kaart3): 
+     if (kaart1.shading == kaart2.shading == kaart3.shading or 
+         (kaart1.shading != kaart2.shading and kaart1.shading != kaart3.shading and kaart2.shading != kaart3.shading)):
+        return True
+     else:
+        return False
+def verglijk(kaart1, kaart2, kaart3):
+    if (verglijk_nummer(kaart1, kaart2 ,kaart3) and verglijk_symbool(kaart1,kaart2, kaart3)
+        and verglijk_kleur(kaart1, kaart2, kaart3) and verglijk_shading(kaart1, kaart2, kaart3)):
+        return True
+    else:
+        return False
+    
+class Kaart:
+    def __init__(self, nummer, symbool, kleur, shading):
+        self.nummer, self.symbool, self.kleur, self.shading = nummer, symbool, kleur, shading    
+    def __str__(self):
+        return "|{self.nummer}, {self.symbool}, {self.kleur}, {self.shading}|".format(self=self)
+    def __repr__(self):
+        return "|{self.nummer}, {self.symbool}, {self.kleur}, {self.shading}|".format(self=self)
+    #we kunnen mogelijk "==" betekenis geven voor onze class om de vergelijkfunctie wat eleganter te maken
+    #def __eq__(self, kaart2):
+        #if self.nummer == kaart2.nummer and 
+
+        
+        
+#twaalf handingevoerdekaarten voor het testen, later hebben we natuurlijk 12 wisselende kaarten
+K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12 = (Kaart(0,0,0,0), Kaart(1,2,2,1), Kaart(1,2,1,0), Kaart(2,1,0,0),Kaart(2,2,2,2),
+                                                     Kaart(1,1,2,1), Kaart(1,0,0,0), Kaart(2,1,2,1), Kaart(0,2,2,2), Kaart(1,1,1,0), Kaart(1,0,2,0), Kaart(0,2,0,1))
+
+twaalfkaarten = [K1, K2, K3, K4, K5, K6, K7, K8, K9, K10, K11, K12]
+
+def elkeSET(lijst):
+    #voor nu een dict ipv een list voor de SETs, voor het geval we andere indices dan gewone getallen willen voor pygame (nog niet naar gekeken), ook meen ik dat dict intern sneller is
+    SETs = {}
+    n = len(lijst)
+    teller = 0
+    for i in range(0, n-2):
+        for j in range(i+1, n-1):
+            for k in range(j+1, n):
+                if verglijk(lijst[i], lijst[j], lijst[k]) == True:
+                    SETs[teller] = lijst[i], lijst[j], lijst[k]
+                    teller += 1
+                else:
+                    pass #voor nu
+    return SETs
+        
+
+print(elkeSET(twaalfkaarten))
---
+++ class kaarten.py
@@ -0,0 +1,58 @@
+#import math, sys
+class Kaart:
+    def __init__(self, nummer, symbool, kleur, shading):
+        self.a = nummer
+        self.b = symbool
+        self.c = kleur
+        self.d = shading
+     
+     #door deze functie worden de as van de kaarten  met elkaar vergleken, als ze hetzelfde zijn of ze zijn allemaal verschillend van elkaar dan gaat het returnen als true en anders is het false   
+    def verglijk_nummer(self, kaart2, kaart3): 
+        if self.a == kaart2.a == kaart3.a or (self.a != kaart2.a and self.a != kaart3.a and kaart2.a != kaart3.a):
+            return True
+        else:
+            return False
+        #door deze functie worden de symbolen van de kaarten met elkaar vergleken, als ze dezelfde zijn of ze zijn allemaal verschillend van elkaar dan gaat het returnen als true en anders is het false
+    def verglijk_symbool(self, kaart2, kaart3):
+        if self.b == kaart2.b == kaart3.b or (self.b != kaart2.b and self.b != kaart3.b and kaart2.b != kaart3.b):
+            return True
+        else:
+            return False
+        #door deze functie worden de kleur van de kaarten met elkaar vergleken, als ze dezelfde zijn of ze zijn allemaal verschillend van elkaar dan gaat het returnen als true en anders is het false
+    def verglijk_kleur(self, kaart2, kaart3):
+        if self.c == kaart2.c == kaart3.c or (self.c != kaart2.c and self.c != kaart3.c and kaart2.c != kaart3.c):
+            return True
+        else: 
+            return False
+    
+    #door deze functie worden de shading van de kaarten met elkaar vergleken, als ze dezelfde zijn of ze zijn allemaal verschillend van elkaar dan gaat het returnen als true en anders is het false
+    def verglijk_shading(self, kaart2, kaart3):
+        if self.d == kaart2.d == kaart3.d or (self.d != kaart2.d and self.d != kaart3.d and kaart2.d != kaart3.d):
+            return True
+        else:
+            return False
+    
+#hier checken we de 3kaarten en kijken of het uiteindelijke waarde true of false is, dit hebben we gedaan doormiddel van de functies met elkaar te verglijken, als alle 4 de functies waarde true geeft dan geeft deze functie ook de waarde true aan en anders geeft het false aan.
+             
+def verglijk(self, kaart2, kaart3):
+    if self.verglijk_nummer(self, kaart2 ,kaart3) and self.verglijk_symbool(kaart2, kaart3) and self.verglijk_kleur(kaart2, kaart3) and self.verglijk_shading(kaart2, kaart3):
+        return True
+    else:
+        return False
+
+
+#zo kan je het testen
+#card1 = Kaart(0,0,0,0)
+#card2 = Kaart(1,1,1,1)
+#card3 = Kaart(2,2,2,2)
+
+#print(card1.verglijk(card2, card3))
+  
+  
+ #we doen dit aan het einde als we tijd hebben en willen veranderen, we halen de functies uit class zelf en voegen __eq__ toe aan de class en alle 4 functies gaan uit de class, later gaan we met __eq__ de true en false doen, dit is aangeraden door een leeraar de andere leeraar zegt dat wat wij aan het doen zijn ook goed is.
+#def verglijk_nummer(kaart1, kaart2, kaart3): 
+     #if kaart1.a == kaart2.a == kaart3.a or (kaart1.a != kaart2.a and kaart1.a != kaart3.a and kaart2.a != kaart3.a):
+        #return True
+     #else:
+        #return False
+        
